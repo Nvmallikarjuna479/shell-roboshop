e52a9ed9a527a56ffe6661282751379f5c78df2b
Venkata Mallikarjuna <nvmalllikarjuna@gmail.com>
2025-10-04 18:44:32 +0530

    getting the private ip address

--- roboshop.sh
+++ roboshop.sh
@@ -9,9 +9,9 @@ do
 
     #get the private ip
     if [ $instance != "forntend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-    else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     fi
     
     echo "INSTANCE IP:: $IP"
